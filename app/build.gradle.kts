37db346478fe07da28885e983e1e1db2a3632c16
Bhushan-714 <bhushanpatil.rcpit@gmail.com>
2024-06-25 22:42:52 +0530

    Commit Successfully

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.github.denzcoskun:ImageSlideshow:0.1.2")
+
+
 }
